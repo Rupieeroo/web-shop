63f2d087a077eef5aae03b3a487f24b756c550fb
Rupert Adams <RupertNCAdams@aol.com>
2019-04-13 14:12:35 +0100

    added first test for prop rendering

--- src/tests/components/products.test.js
+++ src/tests/components/products.test.js
@@ -1,1 +1,11 @@
 import React from 'react';
+import { shallow, mount, render } from 'enzyme';
+import Products from '../../components/products'
+
+describe('Products Component', () => {
+  it('renders the title prop', () => {
+    const wrapper = mount(<Products />);
+    const props = wrapper.props();
+    expect(props.title).toBe("Products");
+  });
+});
