54e0d10ae6898a5bcb3694fd017bc93e766e473e
Rupert Adams <RupertNCAdams@aol.com>
2019-04-13 14:13:51 +0100

    passed title prop render test and added categories

--- src/components/products.js
+++ src/components/products.js
@@ -1,1 +1,27 @@
 import React from 'react';
+
+export default class Products extends React.Component {
+
+  static defaultProps = {
+        title: 'Products',
+        categories: [
+          'Women’s Footwear',
+          'Men’s Footwear',
+          'Women’s Casualwear',
+          'Men’s Casualwear',
+          'Men’s Formalwear',
+          'Women’s Formalwear'
+        ]
+    }
+
+  render() {
+    return (
+      <div>
+        <h1>{this.props.title}</h1>
+        {this.props.categories.map((category, index) => (
+          <p key={index + 1}>{category}</p>
+        )) }
+      </div>
+    );
+  }
+}
