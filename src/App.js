794425fc04828ef7d88eac4e7401ec3d7530d7a2
Rupert Adams <RupertNCAdams@aol.com>
2019-04-13 20:10:54 +0100

    added all components to app for manual testing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 import Products from './components/products';
 import WomenFootwear from './components/women-footwear';
+import MenFootwear from './components/men-footwear';
+import WomenCasual from './components/women-casual';
+import MenCasual from './components/men-casual';
+import MenFormal from './components/men-formal';
+import WomenFormal from './components/women-formal';
 import logo from './logo.svg';
 import './App.css';
 
@@ -11,6 +16,11 @@ class App extends Component {
       <div className="App">
         <Products />
         <WomenFootwear />
+        <MenFootwear />
+        <WomenCasual />
+        <MenCasual />
+        <MenFormal />
+        <WomenFormal />
       </div>
     );
   }
