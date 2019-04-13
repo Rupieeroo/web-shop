d79149252220d69d42e71f41d2cba618a12782f4
Rupert Adams <RupertNCAdams@aol.com>
2019-04-13 19:37:01 +0100

    added WomenFootwear component to App, will be moved later

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import Products from './components/products';
+import WomenFootwear from './components/women-footwear';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
   render() {
     return (
       <div className="App">
         <Products />
+        <WomenFootwear />
       </div>
     );
   }
