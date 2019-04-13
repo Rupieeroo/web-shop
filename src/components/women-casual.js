85490df0584a14cdf167e1d2b798641e2e8cd701
Rupert Adams <RupertNCAdams@aol.com>
2019-04-13 19:50:58 +0100

    added womans casual component

---
+++ src/components/women-casual.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+export default class WomenCasual extends React.Component {
+
+  static defaultProps = {
+    casual: [
+      {
+        id: 1,
+        name: 'Gold Button Cardigan, Black',
+        price: 167.00,
+        stock: 6
+      },
+      {
+        id: 2,
+        name: 'Cotton Shorts, Medium Red',
+        price: 30.00,
+        stock: 5
+      }
+    ]
+  }
+
+  render() {
+
+    const formattedPrice = num => (
+        Intl.NumberFormat('en-UK', { style: 'currency', currency: 'GBP' }).format(num)
+    );
+
+    return(
+      <div>
+      { this.props.casual.map((casual) => (
+        <form key={casual.id}>
+          <h3 key={casual.name}>
+            {casual.name}
+            -
+            {formattedPrice(casual.price)}
+          </h3>
+          <button key={casual.id}>Add to cart</button>
+        </form>
+      ))}
+      </div>
+    );
+  }
+}
