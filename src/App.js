ca4f477cd691ba1aa38d98fe7ff6206c6ae8c539
Rupert Adams <RupertNCAdams@aol.com>
2019-04-13 20:24:08 +0100

    App now soley renders Products

--- src/App.js
+++ src/App.js
@@ -1,11 +1,5 @@
 import React, { Component } from 'react';
 import Products from './components/products';
-import WomenFootwear from './components/women-footwear';
-import MenFootwear from './components/men-footwear';
-import WomenCasual from './components/women-casual';
-import MenCasual from './components/men-casual';
-import MenFormal from './components/men-formal';
-import WomenFormal from './components/women-formal';
 import logo from './logo.svg';
 import './App.css';
 
@@ -15,12 +9,6 @@ class App extends Component {
     return (
       <div className="App">
         <Products />
-        <WomenFootwear />
-        <MenFootwear />
-        <WomenCasual />
-        <MenCasual />
-        <MenFormal />
-        <WomenFormal />
       </div>
     );
   }
