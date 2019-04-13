421cb508f2bb4c881a3757d659e5ff06cf945143
Rupert Adams <RupertNCAdams@aol.com>
2019-04-13 20:09:35 +0100

    added women formal component

---
+++ src/components/women-formal.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+export default class WomenFormal extends React.Component {
+
+  static defaultProps = {
+    formal: [
+      {
+        id: 1,
+        name: 'Bird Print Dress, Black',
+        price: 270.00,
+        stock: 10
+      },
+      {
+        id: 2,
+        name: 'Mid Twist Cut­Out Dress, Pink',
+        price: 540.00,
+        stock: 5
+      }
+    ]
+  }
+
+  render() {
+
+    const formattedPrice = num => (
+        Intl.NumberFormat('en-UK', { style: 'currency', currency: 'GBP' }).format(num)
+    );
+
+    return(
+      <div>
+      { this.props.formal.map((formal) => (
+        <form key={formal.id}>
+          <h3 key={formal.name}>
+            {formal.name}
+            -
+            {formattedPrice(formal.price)}
+          </h3>
+          <button key={formal.id}>Add to cart</button>
+        </form>
+      ))}
+      </div>
+    );
+  }
+}
