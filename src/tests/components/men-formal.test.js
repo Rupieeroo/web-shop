2eb1e6934c1b4ee8f01b376fd91c775c3b15e902
Rupert Adams <RupertNCAdams@aol.com>
2019-04-13 20:02:58 +0100

    added render test for mens formal component

---
+++ src/tests/components/men-formal.test.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import MenFormal from '../../components/men-formal';
+
+describe('Mens Formal', () => {
+  it('renders with formal props', () => {
+    const wrapper = mount(<MenFormal />);
+    const props = wrapper.props();
+    expect(props.casual).toMatchObject(
+      [{
+          id: 1,
+          name: 'Sharkskin Waistcoat, Charcoal',
+          price: 75.00,
+          stock: 2
+        },
+        {
+          id: 2,
+          name: 'Lightweight Patch Pocket Blazer, Deer',
+          price: 175.00,
+          stock: 1
+        }
+      ]);
+  });
+});
