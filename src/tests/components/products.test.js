2cc8ee106d707180950cfe5fbd7d1691b721011e
Rupert Adams <RupertNCAdams@aol.com>
2019-04-13 14:56:27 +0100

    added test for rendering categories props

--- src/tests/components/products.test.js
+++ src/tests/components/products.test.js
@@ -8,4 +8,18 @@ describe('Products Component', () => {
     const props = wrapper.props();
     expect(props.title).toBe("Products");
   });
+
+
+    it ('renders 6 category components', () => {
+      const wrapper = mount(<Products />);
+      const props = wrapper.props();
+      expect(props.categories).toEqual(
+        ["Women’s Footwear",
+        "Men’s Footwear",
+        "Women’s Casualwear",
+        "Men’s Casualwear",
+        "Men’s Formalwear",
+        "Women’s Formalwear"
+      ]);
+    });
 });
