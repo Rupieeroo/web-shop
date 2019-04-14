0313fa94df854558c0ff39492ed570b2c2c6023a
Rupert Adams <RupertNCAdams@aol.com>
2019-04-14 16:55:14 +0100

    added 'add to basket functionality to womens footwear

--- src/components/women-footwear.js
+++ src/components/women-footwear.js
@@ -19,25 +19,40 @@ export default class WomenFootwear extends React.Component {
     ]
   }
 
+  handleAddToBasket = (e) => {
+      e.preventDefault();
+  }
+
+  handleShoeSubmit = (shoe) => {
+      return () => this.props.handleAddToBasket(shoe)
+  }
+
   render() {
 
     const formattedPrice = num => (
         Intl.NumberFormat('en-UK', { style: 'currency', currency: 'GBP' }).format(num)
     );
 
+    const shoeList = ( this.props.shoes.map((shoe) => (
+      <form key={shoe.id}
+            value={shoe}
+            onSubmit={this.handleShoeSubmit(shoe)}
+      >
+        <h3 key={shoe.name}>
+          {shoe.name}
+          -
+          {formattedPrice(shoe.price)}
+        </h3>
+        <button
+         key={shoe.id}
+         value={shoe}
+         >Add to cart</button>
+      </form>
+    )));
+
     return(
       <div>
-        <h2>Women's Footwear</h2>
-        { this.props.shoes.map((shoe) => (
-          <form key={shoe.id}>
-            <h3 key={shoe.name}>
-              {shoe.name}
-              -
-              {formattedPrice(shoe.price)}
-            </h3>
-            <button key={shoe.id}>Add to cart</button>
-          </form>
-        ))}
+        { shoeList }
       </div>
     );
   }
