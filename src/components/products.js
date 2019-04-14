85ce52d22f13cd441977b25398629ce9c883a1df
Rupert Adams <RupertNCAdams@aol.com>
2019-04-14 12:12:28 +0100

    began connecting basket for user stories

--- src/components/products.js
+++ src/components/products.js
@@ -5,17 +5,32 @@ import WomenCasual from './women-casual';
 import MenCasual from './men-casual';
 import MenFormal from './men-formal';
 import WomenFormal from './women-formal';
+import Basket from './basket';
 
 export default class Products extends React.Component {
+
+  state = {
+    basket: [{ item: "thing" }],
+    selectedItem: undefined
+  }
 
   static defaultProps = {
         title: 'Products',
     }
 
+    handleEmptyBasket = () => {
+      console.log('basket has been emptied');
+      this.setState(() => ({ basket: [] }));
+    };
+
+
   render() {
     return (
       <div>
         <h1>{this.props.title}</h1>
+        <Basket
+          handleEmptyBasket={this.handleEmptyBasket}
+        />
         <WomenFootwear />
         <MenFootwear />
         <WomenCasual />
