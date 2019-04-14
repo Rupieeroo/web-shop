b9155bc6ea6d8e98b62c78a539817140e93c8b4b
Rupert Adams <RupertNCAdams@aol.com>
2019-04-14 12:12:56 +0100

    tested empty basket button on state

--- src/components/basket.js
+++ src/components/basket.js
@@ -1,1 +1,12 @@
 import React from 'react';
+
+const Basket = (props) => (
+  <div className="basket">
+    <h3>Basket</h3>
+    <button
+      onClick={props.handleEmptyBasket}
+    >Remove All Items</button>
+  </div>
+);
+
+export default Basket;
