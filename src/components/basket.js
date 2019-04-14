a930b0377470e7b2d98d1512397e4291700538ad
Rupert Adams <RupertNCAdams@aol.com>
2019-04-14 17:26:15 +0100

    added delete from basket event functionality to basket

--- src/components/basket.js
+++ src/components/basket.js
@@ -3,9 +3,16 @@ import React from 'react';
 const Basket = (props) => (
   <div className="basket">
     <h3>Basket</h3>
-    {props.basket === 0 && <p className="empty__message">Shopping cart is empty.</p>}
+    {props.basket === [] && <p className="empty__message">Shopping cart is empty.</p>}
     {props.basket.map((item, index) => (
-      <p key={index + 1}>{item.name}</p>
+      <div key={item.id}>
+        <p key={index + 1}>{item.name}</p>
+        <button
+            onClick={() => {props.handleDeleteFromBasket(item)}}
+        >
+          Remove From Basket
+        </button>
+      </div>
     ))}
     <button
       onClick={props.handleEmptyBasket}
